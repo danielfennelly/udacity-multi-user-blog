0c904cb85544610af0d8890a4b3826053cfebd15
danielfennelly <daniel.fennelly@gmail.com>
2016-08-01 17:17:26 -0700

    made validators.py pep8 compliant

--- validators.py
+++ validators.py
@@ -4,20 +4,23 @@ USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASSWORD_RE = re.compile(r"^.{3,20}$")
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
+
 def validate_username(username):
     if USER_RE.match(username):
-    	return username
+        return username
     else:
-    	return None
+        return None
+
 
 def validate_password(password, verify):
-	if PASSWORD_RE.match(password) and password == verify:
-		return password
-	else:
-		return None
+    if PASSWORD_RE.match(password) and password == verify:
+        return password
+    else:
+        return None
+
 
 def validate_email(email):
-	if EMAIL_RE.match(email):
-		return email
-	else:
-		return None
+    if EMAIL_RE.match(email):
+        return email
+    else:
+        return None
